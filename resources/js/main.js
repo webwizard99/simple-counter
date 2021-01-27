f9d00439ab9d921d331b6279e0b478cc4ae8b72c
Michael Bragg <michael_bragg99@hotmail.com>
2021-01-27 04:55:01 -0700

    Add select control to control which counters are added.

--- resources/js/main.js
+++ resources/js/main.js
@@ -14,11 +14,27 @@ const storage = (function(){
 const counterController = (function(){
   const DOM = {
     countersContainer: '.counters-container',
-    newButton: '.new-button'
+    newButton: '.new-button',
+    newButtonContainer: '.new-button-container',
+    newSelect: '.new-button-select'
   }
-
+  const newButtonContainerStart = `
+  <div class="new-button-container">
+  `
   const newButton = `
     <div class="new-button button noselect"><p class="new-button-icon noselect">+</p></div>
+  `
+  const newSelectStart = `
+  <select class="new-button-select">
+  `
+  const newOptionTemplate = `
+  <option class="new-option" key="%key%" value="%count-type%">%type-display%</option>
+  `
+  const newSelectEnd = `
+  </select>
+  `
+  const newButtonContainerEnd = `
+  </div>
   `
   const counterTemplate = `
     <div id="%id%" class="counter-container button noselect" counter-id="%count-id%">
@@ -87,7 +103,23 @@ const counterController = (function(){
     container.removeChild(document.querySelector(thisDom));
     counters = counters.filter(counter => counter.id !== this.id);
     countersReserve.push(reserveMap[this.counterType]);
-    
+  }
+
+  getNewButton = () => {
+    let newButtonTemplate = newButtonContainerStart + newButton;
+    newButtonTemplate += newSelectStart;
+    if (countersReserve.length > 0) {
+      countersReserve.forEach(counterReserve => {
+        let newOption = newOptionTemplate;
+        newOption = newOption.replace('%key%', counterReserve);
+        newOption = newOption.replace('%count-type%', counterReserve);
+        newOption = newOption.replace('%type-display%', countersMap[counterReserve]);
+        newButtonTemplate += newOption
+      });
+    }
+    newButtonTemplate += newSelectEnd;
+    newButtonTemplate += newButtonContainerEnd;
+    return newButtonTemplate;
   }
 
   return {
@@ -95,8 +127,10 @@ const counterController = (function(){
       // exit if max counters exist
       if (counters.length >= maxCounters) return;
       // create counter model
-      const newCountType = countersReserve.shift();
-      const newCounter = new Counter(countersMap[newCountType], 0);
+      const currentType = Number.parseInt(document.querySelector(DOM.newSelect).value);
+      countersReserve = countersReserve.filter(reserveCounter => reserveCounter !== currentType);
+      // const newCountType = countersReserve.shift();
+      const newCounter = new Counter(countersMap[currentType], 0);
       counters.push(newCounter);
       // compose DOM for counter
       let newTemplate = counterTemplate.slice();
@@ -106,13 +140,28 @@ const counterController = (function(){
       newTemplate = newTemplate.replace("%count%", newCounter.value);
       // manage new-button and new counter DOM in container
       let container = document.querySelector(DOM.countersContainer);
-      const newButtonDOM = document.querySelector(DOM.newButton);
+      const newButtonDOM = document.querySelector(DOM.newButtonContainer);
       if (newButtonDOM) {
         container.removeChild(newButtonDOM);
       }
+      const newButtonHTML = getNewButton();
       container.innerHTML += newTemplate;
       if (counters.length < maxCounters) {
-        container.innerHTML += newButton;
+        container.innerHTML += newButtonHTML;
+        relinkNewButton();
+      }
+      
+    },
+    addNewButton: () => {
+      // manage new-button and new counter DOM in container
+      let container = document.querySelector(DOM.countersContainer);
+      const newButtonDOM = document.querySelector(DOM.newButtonContainer);
+      if (newButtonDOM) {
+        container.removeChild(newButtonDOM);
+      }
+      const newButtonHTML = getNewButton();
+      if (counters.length < maxCounters) {
+        container.innerHTML += newButtonHTML;
       }
     },
     clearCounters: () => {
@@ -126,9 +175,10 @@ const counterController = (function(){
     },
     incrementCounter: (e) => {
       console.log(e.target);
-      e.preventDefault();
+      // e.preventDefault();
       let targetCounter = e.target;
       if (targetCounter.classList.contains('main-container')) return;
+
       if (!e.target.classList.contains('counter-container')) {
         targetCounter = targetCounter.parentNode;
       }
@@ -144,8 +194,12 @@ const counterController = (function(){
       console.log(counters);
       let foundCounter = counters.find(counter => counter.id === counterId);
       console.log(foundCounter);
+      if (!foundCounter) return;
       foundCounter.incrementCounter();
       foundCounter.updateContent();
+    },
+    handleSelect: (e) => {
+
     }
   }
 
@@ -163,9 +217,13 @@ const setEventListeners = () => {
   document.querySelector(buttons.clear).addEventListener('touchstart', clearCounts);
 }
 
+const relinkNewButton = () => {
+  const DOM = counterController.getDOMStrings();
+  document.querySelector(DOM.newButton).addEventListener('touchstart', counterController.addCounter);
+}
+
 const init = () => {
-  counterController.addCounter();
-  counterController.addCounter();
+  counterController.addNewButton();
   setEventListeners();
 }
 
